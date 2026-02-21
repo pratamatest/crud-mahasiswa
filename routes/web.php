674dc4ff66481f1e2c2da6ed38ceb196e4466f37
PratamaMF <fajarpratamavpc@gmail.com>
2026-02-22 01:45:14 +0800

    tambah route migrasi

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/migrasi-db', function() {
+    Artisan::call('migrate:fresh --force');
+    return "Database berhasil diupdate! Silakan kembali ke halaman utama.";
+});
 Route::get('/', [MahasiswaController::class, 'index']);
 Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
 Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
